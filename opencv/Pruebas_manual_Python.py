090bbd77f2ae24db402630f1e16462aa25917bc9
EzeCanzani <132935212+EzeCanzani@users.noreply.github.com>
2023-11-24 15:12:31 -0300

    Pruebas_manual_Python

--- opencv/Pruebas_manual_Python.py
+++ opencv/Pruebas_manual_Python.py
@@ -105,3 +105,121 @@ resultado_formateado = "{:.2f} Chinese Yuan".format(resultado)
 
 # Imprime el resultado formateado
 print(resultado_formateado)
+
+
+#Redondeo de númers
+
+import math
+
+numero= 8.75
+
+numero_redondeado=math.ceil(numero)
+
+print(numero_redondeado)
+
+msg=("intento de salto de primera linea \n a segunda linea")
+print (msg)
+
+msg=("valor de x=65")
+print (msg)
+
+#calculo del tercer angulo de un triangulo
+
+def third_angle(angle1, angle2):
+    # Calculate the third angle
+    angle3 = 180 - angle1 - angle2
+    
+    return angle3
+
+# Example usage:
+angle1 = 70
+angle2 = 35
+
+result = third_angle(angle1, angle2)
+print(f"The third angle is: {result} degrees")
+
+name= "Elion Musk"
+age= 49
+fortune= 4000
+
+msg= f"Me llamo {name}, tengo {age} años de edad, y una fortuna de {fortune} millones"
+print(msg)
+
+a=10
+print(a)
+
+#Ejercicios STRINGS
+c = 2.71828
+print(f"{c:.3f}")
+
+print(f"{c:.6f}")
+
+print(f"{c:8.2f}")
+
+print(f"{c:e}")
+
+print(f"{c:010.4f}")
+
+print(f"{c:19.5f}")
+
+
+#If
+
+value = 23
+
+if value is not None:
+    print(f"value={value}")
+
+#Sentencia Match_case
+
+def calcular_resultado(num1, num2, operando):
+    resultado = None
+
+    match operando:
+        case '+':
+            resultado = num1 + num2
+        case '-':
+            resultado = num1 - num2
+        case '*':
+            resultado = num1 * num2
+        case '/':
+            resultado = num1 / num2
+        case _:
+            print("Operación no válida. Por favor, elija una de las siguientes: '+', '-', '*', '/'")
+
+    return resultado
+
+# Solicitar valores al usuario
+num1 = float(input("Ingrese el primer valor numérico: "))
+num2 = float(input("Ingrese el segundo valor numérico: "))
+operando = input("Ingrese el operando (+, -, *, /): ")
+
+# Calcular el resultado
+resultado = calcular_resultado(num1, num2, operando)
+
+# Mostrar el resultado si la operación es válida
+if resultado is not None:
+    print(f"{num1} {operando} {num2} = {resultado}")
+
+#Ejercicios Pág 127
+A
+def get_weekday(number):
+    if 1 <= number <= 7:
+        weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        return weekdays[number - 1]
+    else:
+        return "Wrong, please enter a number between 1 and 7"
+B
+# Example usage:
+number_input = int(input("Enter a number between 1 and 7: "))
+result = get_weekday(number_input)
+print(result)
+
+C
+def make_negative(number):
+    return -abs(number)
+
+# Example usage:
+input_number = float(input("Enter a number: "))
+result = make_negative(input_number)
+print(f"The negative version of {input_number} is {result}")
